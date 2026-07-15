309c09a44d7209918eb5c460c35f3812d6a7f74e
Riccardo <riccardograndicelli03@gmail.com>
2026-07-15 14:34:14 +0200

    Implemented gene expression standardization

--- functions/datatransf/ml_functions.py
+++ functions/datatransf/ml_functions.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from scipy.sparse import load_npz, hstack, vstack
+from scipy.sparse import load_npz, hstack, csr_array
 import numpy as np
 import pandas as pd
 from collections import Counter
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
 
 __author__=['Riccardo Grandicelli']
 __email__=['riccardograndicelli03@gmail.com']
 
 
-def weighted_train_test_split(drug: str, features: list, test_size: float, random_state: int = None):
+def weighted_train_test_split(drug: str, features: list, test_size: float, standardize: bool = False, random_state: int = None):
     '''
     Given a drug and one or more set of features (gene expression, gpa or snps), this function divides those features data 
-    into train and test set, keeping in both sets the same proportion between susceptible and resistant to a certain drug. 
+    into train and test set, keeping in both sets the same proportion between susceptible and resistant to a certain drug.
+
+    If required, this function can also standardize gene expression data (they are already standardized, but before the division
+    between train and test sets, so train and test data are not really independent). Gpa and snps data don't need to be standardized
+    because they are binary data (either 0 or 1).
 
     Parameters
     ----------
@@ -26,6 +31,8 @@ def weighted_train_test_split(drug: str, features: list, test_size: float, rando
             element in the list is not one of these three, the code raises a ValueError.
         test_size: float
             The percentage of dataset that will be used as test set.
+        standardize: bool (default: False)
+            If True, standardize gene expression data after the train-test division.
         random_state: int (default: None)
             Random seed for reproducibility.
     Returns
@@ -58,27 +65,11 @@ def weighted_train_test_split(drug: str, features: list, test_size: float, rando
     Y_susceptible = targets[targets[drug]<0.5]
     Y_resistent = targets[targets[drug]>0.5]
 
-    #merge data for the required set of features into a single sparse matrix
-    for i, feature in enumerate(Counter(features).keys()): #keep only unique values of the list of features, to avoid issues with repetitions
-        if feature not in ['genexp', 'gpa', 'snps']:
-            raise ValueError("Set of features chosen is not one of the possible choices")
-        if i==0:
-            #initialize the csr_array of input features
-            features = load_npz("./transformed_data/features/" + feature + "_features.npz")
-        else:
-            #from the second iteration, stack new csr_array of input features column-wise (horizontally)
-            features = hstack([features, load_npz("./transformed_data/features/" + feature + "_features.npz")], format = "csr")
-    
-    #Divide features dataset into strains susceptible and resistant to the chosen drug
-    X_susceptible = features[Y_susceptible["Index"].tolist()]
-    X_resistent = features[Y_resistent["Index"].tolist()]
-        
-    X_train_s, X_test_s, Y_train_s, Y_test_s = train_test_split(X_susceptible, Y_susceptible, test_size=test_size, random_state=random_state)
-    X_train_r, X_test_r, Y_train_r, Y_test_r = train_test_split(X_resistent, Y_resistent, test_size=test_size, random_state=random_state)
+    #first split only the targets dataset, the input features will be splitted separately, so that gene expression data can be standardized
+    #after the division
+    Y_train_s, Y_test_s = train_test_split(Y_susceptible, test_size=test_size, random_state=random_state)
+    Y_train_r, Y_test_r = train_test_split(Y_resistent, test_size=test_size, random_state=random_state)
 
-    X_train=vstack([X_train_s, X_train_r], format = "csr")
-    X_test=vstack([X_test_s, X_test_r], format = "csr")
-    
     #separate output classes from "Index" and "Strain" columns
     Y_train_full=pd.concat([Y_train_s, Y_train_r]) #three columns: drug, index and strain
     Y_test_full=pd.concat([Y_test_s, Y_test_r])
@@ -87,5 +78,34 @@ def weighted_train_test_split(drug: str, features: list, test_size: float, rando
     Y_test = np.array(Y_test_full[drug], dtype = np.float64)
     strains_train = Y_train_full[["Index", "Strain"]]
     strains_test = Y_test_full[["Index", "Strain"]]
+
+    #get the indexes of train and test samples, to split also input data
+    train_indexes = strains_train["Index"].tolist()
+    test_indexes = strains_test["Index"].tolist()
+
+    for i, feature in enumerate(Counter(features).keys()): #keep only unique values of the list of features, to avoid issues with repetitions
+        if feature not in ['genexp', 'gpa', 'snps']:
+            raise ValueError("Set of features chosen is not one of the possible choices")
+        
+        #create a sparse matrix for each group of features
+        new_features = load_npz("./transformed_data/features/" + feature + "_features.npz")
+        
+        #divide into train and test, using the same division of the output classes
+        new_features_train = new_features[train_indexes]
+        new_features_test = new_features[test_indexes]
+
+        if standardize and feature == 'genexp':
+            new_features_train = scale(new_features_train.toarray()) #standardization cannot be done using sparse matrices
+            new_features_test = scale(new_features_test.toarray()) #so we convert into np.ndarray
+            new_features_train = csr_array(new_features_train)
+            new_features_test = csr_array(new_features_test)
+        
+        #append different features data together
+        if i == 0:
+            X_train = new_features_train
+            X_test = new_features_test
+        else:
+            X_train = hstack([X_train, new_features_train], format = "csr")
+            X_test = hstack([X_test, new_features_test], format = "csr")
 
     return X_train, X_test, Y_train, Y_test, strains_train, strains_test
